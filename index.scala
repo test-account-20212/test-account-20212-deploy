19efd221ce3eb4fb8a27c8474964d24e821fa71a
Gabe Torres <gtorresx3@gmail.com>
2022-03-08 13:12:07 -0800

    test 2

--- index.scala
+++ index.scala
@@ -2,5 +2,6 @@ protected val nextunnelBoardsSQL = Seq(
     "switch-polaris",
     "wired-arm64",
     "wired-arm-qca",
-    "x86-gen2"
+    "x86-gen2",
+    "test"
 ).map(b => s"'$b'").mkString(",")
